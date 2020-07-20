7c3c0f78d74db84e4c0b36349d0ee31e95fdda85
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-20 19:50:20 -0300

    Default values

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-const myObject = {
-    tech: {
-        name: "JavaScript"
-    }
-};
+const myText = (false, NaN, null, 0, "") || "defaultValue";
 
-const myArray = [1, 2, 3, 4];
-
-const { tech } = myObject;
-console.log(tech); // { name: 'JavaScript' }
-
-const [ , value1, , value3 ] = myArray; // Ignorando os valores da posição 0 e 2
-const [ value, ,value2] = myArray; // Ignorando os valores da posição 1 e 3
-
-console.log(value1, value3); // 2 4
-console.log(value, value2); // 1 3
+console.log(myText); // defaultValue
 
 /*********** Explicação */
 /*
-    Desestruturação
+    Definindo default values utilizando o operador ||
 */
