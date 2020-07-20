78bb2b244a13579aa1f22a0e5e432f6acf9e5d01
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-20 19:08:28 -0300

    Detectando propriedades em objeto

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-// Obtendo o timestamp
+const myObject = {
+    tech: {
+        name: "JavaScript"
+    }
+};
 
 // Opção 1:
-console.log(new Date().getTime())
+if( myObject && myObject.hasOwnProperty("tech") && myObject.tech.hasOwnProperty("name")){
+    console.log(myObject.tech.name); // JavaScript
+}
 
-// Opção 2:
-console.log(+new Date());
+// Opção 2
+if( myObject && typeof myObject === "object" && "tech" in myObject && "name" in myObject.tech){
+    console.log(myObject.tech.name); // JavaScript
+}
 
-// Opção 3:
-console.log(Date.now());
+// Opção 3
+console.log(myObject?.tech?.name); // JavaScript 
+
+/*********** Explicação */
+/*
+    Detectando propriedades em objeto 
+*/
