00bbb0c7b1a463c99ce92bb07226c53d68d7710b
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-20 14:22:47 -0300

    Avaliaçao de Curto-Circuito (Short-Circuit)

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const text = "12345";
-const text1 = null;
-const text2 = "AEIOU";
+function isOk(){
+    return true;
+}
 
-console.log(`text: ${text} - Tipo: ${typeof text}`)         // text: 12345 - Tipo: string
-console.log(`text: ${+text} - Tipo: ${typeof +text}`)       // text: 12345 - Tipo: number
+function logMessage(message) {
+    console.log(message);
+    return true;
+}
 
-console.log(`text: ${text1} - Tipo: ${typeof text1}`)       // text1: null - Tipo: Object
-console.log(`text: ${+text1} - Tipo: ${typeof +text1}`)     // text1: 0 - Tipo: number
+function logInfo(message){
+    console.info(message);
+    return Nan;
+}
 
-console.log(`text: ${text2} - Tipo: ${typeof text2}`)       // text2: AEIOU - Tipo: string
-console.log(`text: ${+text2} - Tipo: ${typeof +text2}`)     // text2: NaN - Tipo: number
+function logError(message){
+    console.error(message);
+    return true;
+}
+
+// Ao invés disso:
+if(isOk()){
+    logMessage("My log");
+    if(logInfo("My info")){
+        logError("My error");
+    };
+}
+
+// Faça:
+isOk() && logMessage("My log") && logInfo("My info") && logError("My Error");
 
 /*********** Explicação */
 /*
-    +Var -> Este é o operador que pode ser utilizado para converter variáveis para númerico.
-    --> Quando nulo o valor é convertido para 0
-    --> Quando texto (ex: AEIOU) o valor e convertido para NaN  
+    Avaliação de Curto-Circuito (Short-Circuit).
+    --> A avaliação acontece através das expressões. Neste caso o AND.
+    --> A função "isOk()" retornou um valor verdadeiro, possibilitando então a execução da segunda função "logMessage()"
+    --> A função "logMessage()" retornou um valor verdadeiro, possibilitando então a execução da terceira função "logInfo()"
+    --> A função "logInfo()" retornou um valor falso, resultando no final da execução do script
 */
