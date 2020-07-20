e75b3a955c3c4cd21c8bcc1d632572d7d04d10f3
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-20 18:41:15 -0300

    Obtendo o timestamp

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
-function isOk(){
-    return true;
-}
+// Obtendo o timestamp
 
-function logMessage(message) {
-    console.log(message);
-    return true;
-}
+// Opção 1:
+console.log(new Date().getTime())
 
-function logInfo(message){
-    console.info(message);
-    return null;
-}
+// Opção 2:
+console.log(+new Date());
 
-function logError(message){
-    console.error(message);
-    return true;
-}
-
-// Ao invés disso:
-if(isOk()){
-    logMessage("My log");
-    if(logInfo("My info")){
-        logError("My error");
-    };
-}
-
-/* Saída */
-// My log
-// My info
-
-// Faça:
-isOk() && logMessage("My log") && logInfo("My info") && logError("My Error");
-
-/* Saída */
-// My log
-// My info
-
-/*********** Explicação */
-/*
-    Avaliação de Curto-Circuito (Short-Circuit).
-    --> A avaliação acontece através das expressões. Neste caso o AND.
-    --> A função "isOk()" retornou um valor verdadeiro, possibilitando então a execução da segunda função "logMessage()"
-    --> A função "logMessage()" retornou um valor verdadeiro, possibilitando então a execução da terceira função "logInfo()"
-    --> A função "logInfo()" retornou um valor falso, resultando no final da execução do script
-*/
+// Opção 3:
+console.log(Date.now());
