c1dc50ab34f3d530b0fc6f3777e113451c77f5dc
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-21 18:28:52 -0300

    Array - Utilizando o filter

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const names = ["Leonardo", "Ana", "João", "William", "Joyce", "Gabriel", "Paula"];
+const list = [
+    {name: "Leonardo", age: 16}, 
+    {name: "Ana Paula", age: 25}, 
+    {name: "João", age: 15}, 
+    {name: "William", age: 26}, 
+    {name: "Joyce", age: 35}, 
+    {name: "Gabriel", age: 25}, 
+    {name: "Paula", age: 45}, 
+    {name: "Ana Julia", age: 10}
+];
 
-function shuffle(list){
-    return list.sort(() => Math.random() - 0.5);
-}
-
-console.log(shuffle(names));
-
-/*********** Explicação */
-/*
-    Definindo default values utilizando o operador ||
-*/
+// Filtrando os nomes que começam a letra 'A'
+const newList = list.filter(({name}) => name.toLowerCase().startsWith("a"));
+console.log(newList); // [ { name: 'Ana Paula', age: 25 }, { name: 'Ana Julia', age: 10 } ]
