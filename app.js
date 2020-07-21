82a87c2e48ad739d6419a2f9781de760a524113b
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-21 18:49:01 -0300

    Array - Utilizando o sort

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const list = [
     {name: "Ana Julia", age: 10}
 ];
 
-// Filtrando os nomes que começam a letra 'A'
-const newList = list.filter(({name}) => name.toLowerCase().startsWith("a"));
-console.log(newList); // [ { name: 'Ana Paula', age: 25 }, { name: 'Ana Julia', age: 10 } ]
+// Ordenando lista por ordem alfabética
+const newList = list.sort((a, b) => a.name > b.name ? 1 : -1);
+console.log(newList);
+/*
+    [
+    { name: 'Ana Julia', age: 10 },
+    { name: 'Ana Paula', age: 25 },
+    { name: 'Gabriel', age: 25 },
+    { name: 'Joyce', age: 35 },
+    { name: 'João', age: 15 },
+    { name: 'Leonardo', age: 16 },
+    { name: 'Paula', age: 45 },
+    { name: 'William', age: 26 }
+    ]
+*/
