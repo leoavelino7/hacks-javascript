1a4c09720ea258a273a2c85b6861c90f1d27d77e
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-07-22 20:03:21 -0300

    Desestruturaçao em funçoes

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const values = [
-    55,
-    100,
-    58,
-    95,
-    78,
-    145,
-    789,
-    250
-];
+function myFunction({username, password}){
+    console.log("Username:", username); // Username: @ehash
+    console.log("Password:", password); // Password: ilovejs
+};
 
-const maxValue = Math.max(...values);
-console.log(maxValue); // 789
+const myData = {
+    username: "@ehash",
+    password: "ilovejs"
+};
 
-const minValue = Math.min(...values);
-console.log(minValue); // 55
+myFunction(myData);
